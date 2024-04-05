50b503d03aac946675ed4f9c037d697470324eb5
Leonardo <leopaulasilva83@gmail.com>
2024-04-05 20:06:48 -0300

    add readme login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrlBackend: 'http://ec2-3-135-222-28.us-east-2.compute.amazonaws.com:8080/app/api/',
+    baseUrlBackend: 'http://ec2-3-135-222-28.us-east-2.compute.amazonaws.com:8080/app/api',
     token: 'token'
   };
